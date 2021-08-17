6c90c7e8791ea43f1ce42b55760ebe66201e2bf6
Gabriel Schreiner Bueno de Camargo <gsbcamargo@gmail.com>
2021-08-17 14:58:41 -0300

    modified

--- gsbc.js
+++ gsbc.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 
-"use strict";
-const { readFile } = require("fs");
-const { join } = require("path");
+'use strict'
+const { readFile } = require('fs')
+const { join } = require('path')
 
-readFile(join(__dirname, "gabInfo.json"), "utf8", (err, content) => {
-  if (err) throw err;
-  console.log(content);
-});
+readFile(join(__dirname, 'info.json'), 'utf8', (err, content) => {
+  if (err) throw err
+  console.log(content)
+})
